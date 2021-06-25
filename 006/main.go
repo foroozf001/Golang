7144d9f2cde2f474cd89d7383af73172353750b6
ffo <farazforoozan@gmail.com>
2021-06-25 16:40:19 +0200

    Slices

--- 006/main.go
+++ 006/main.go
@@ -9,5 +9,25 @@ func main() {
 func NinjaLevel4_exercise1() {
 	// slice
 	x := []int{1, 2, 6, 10}
-	fmt.Printf("%v\n", x[1:3])
+	for i, v := range x {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	// append elements
+	x = append(x, 12, 13)
+	fmt.Printf("%v %d\n", x, len(x))
+
+	// join two slices
+	y := []int{-1, -2}
+	x = append(x, y...)
+	fmt.Printf("%v %d\n", x, len(x))
+
+	// delete elements
+	x = append(x[:2], x[4:]...)
+	fmt.Println(x)
+
+	// make
+	z := make([]int, 4, 10)
+	// there's 4 elements in the slice and there's 10 spots in total in the underlying array
+	fmt.Printf("slice: %v\t length: %d\t capacity: %d\n", z, len(z), cap(z))
 }
