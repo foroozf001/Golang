5f0b117fade0ae02167da1622da1b18145fb3c5e
ffo <farazforoozan@gmail.com>
2021-07-07 20:46:47 +0200

    functions

--- 008_functions/main.go
+++ 008_functions/main.go
@@ -2,146 +2,63 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// Ninja Level 6 exercise 1
-	f := foo1()
-	a, b := bar1()
-	fmt.Println(f, a, b)
-
-	// Ninja Level 6 exercise 2
-	ninja2 := []int{1, 8, 3}
-	c := foo2(ninja2...) //extract values
-	fmt.Println(c)
-
-	d := bar2(ninja2)
-	fmt.Println(d)
-
-	// Ninja Level 6 exercise 3
-	defer foo3() //deferred function
-	fmt.Println("This function is placed after foo3")
-
-	// Ninja Level 6 exercise 4
-	p1 := person{
-		first: "Bob",
-		last:  "Ross",
-		age:   50,
-	}
-	p1.speak()
-	// Ninja Level 6 exercise 5
-	circ := circle{
-		radius: 2.0,
-	}
-	squa := square{
-		length: 2.0,
-	}
-	info(circ)
-	info(squa)
-
-	// Ninja Level 6 exercise 6
-	func() {
-		fmt.Println("This is a an anonymous function")
-	}()
-
-	// Ninja Level 6 exercise 7
-	fcc := firstClassCitizen
-	fmt.Println(fcc())
-
-	// Ninja Level 6 exercise 8
-	fret := fret()
-	fmt.Println(fret())
-
-	// Ninja Level 6 exercise 9
-	mff := func(f func() int) int {
-		return f() * -42
-	}
-	fmt.Println(mff(callb))
+	NinjaLevel6_exercise1(foo, bar)
+	NinjaLevel6_exercise2()
+	NinjaLevel6_exercise3()
 }
 
 // Ninja Level 6 exercise 1
-func foo1() int {
-	return 123
+func NinjaLevel6_exercise1(f1 func() int, f2 func() (string, int)) {
+	r0 := f1()
+	r1, r2 := f2()
+	fmt.Println(r0, r1, r2)
 }
 
-func bar1() (int, string) {
-	return 456, "bar"
+func foo() int {
+	return 7
+}
+
+func bar() (string, int) {
+	return "bar", 8
 }
 
 // Ninja Level 6 exercise 2
-func foo2(sum ...int) int { //input is variadic parameter
-	total := 0
-	for _, i := range sum {
-		total += i
-	}
-	return total
+func NinjaLevel6_exercise2() {
+	var my_slice []int
+	my_slice = []int{1, 2, 7, 31}
+
+	fmt.Printf("%d\n", sum(even, my_slice...))
 }
 
-func bar2(sum []int) int { //input is slice of int
-	total := 0
-	for _, i := range sum {
-		total += i
+func even(in int) int {
+	if in%2 == 0 {
+		return in
+	} else {
+		return 0
 	}
-	return total
+}
+
+func sum(f func(in int) int, values ...int) int {
+	ii := []int{}
+	sum := 0
+
+	// Add all even numbers to new slice
+	for _, val := range values {
+		ii = append(ii, f(val))
+	}
+
+	// Sum all values in slice containing even numbers
+	for _, val := range ii {
+		sum += val
+	}
+	return sum
 }
 
 // Ninja Level 6 exercise 3
-func foo3() {
-	fmt.Println("Foo3 is deferred")
-}
-
-// Ninja Level 6 exercise 4
-type person struct {
-	first string
-	last  string
-	age   int
-}
-
-func (p person) speak() {
-	fmt.Println("Hi, my name is", p.first, p.last, "and I'm", p.age, "years old")
-}
-
-// Ninja Level 6 exercise 5
-type shape interface {
-	area() float64
-}
-
-type circle struct {
-	shape
-	radius float64
-}
-
-type square struct {
-	shape
-	length float64
-}
-
-func (c circle) area() float64 {
-	return math.Pi * math.Pow(c.radius, 2)
-}
-
-func (s square) area() float64 {
-	return math.Pow(s.length, 2)
-}
-
-func info(s shape) {
-	fmt.Println(s.area())
-}
-
-// Ninja Level 6 exercise 7
-func firstClassCitizen() string {
-	return "This is a function stored in a variable"
-}
-
-// Ninja Level 6 exercise 8
-func fret() func() int {
-	return func() int {
-		return 404
-	}
-}
-
-// Ninja Level 6 exercise 9
-func callb() int {
-	return -3
+func NinjaLevel6_exercise3() {
+	defer fmt.Println("Deferred")
+	fmt.Println(foo())
 }
